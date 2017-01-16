4a2aee1d04c291d181161563ab70459ff3472d59
infantopratik <infanto.pratik@gmail.com>
2017-01-16 10:48:44 +0530

    Images and view file for Leadership Menu

---
+++ application/views/leadership.php
@@ -0,0 +1,54 @@
+
+
+<!--Page Header-->
+<section class="page_header child_nursing padding-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 page-content">
+        <h1>Our Leadership</h1>
+        <div class="page_nav">
+          <span>You are here:</span> <a href="<?php echo base_url() ?>">Home</a> <span><i class="fa fa-angle-double-right"></i>Our Leadership</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Page Header-->
+
+
+<!-- Courses -->
+<section id="course_all" class="padding-bottom-half padding-top">
+  <div class="container content-container">
+    <div class="row">
+      <div class="col-sm-10 col-sm-offset-1 course_detail wow fadeIn" data-wow-delay="400ms">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="<?php echo base_url()?>/images/leaders/chancellor- NM Veeraiyan.jpg" alt="">
+            <h3>Chancellor: Dr.N.MVeeraiyan</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="<?php echo base_url()?>/images/leaders/Dirctor-Thiyagarajan.jpg" alt="">
+            <h3>Registrar: Dr.Thiyagarajan</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="<?php echo base_url()?>/images/leaders/Director Saveetha Rajesh.jpg" alt="">
+            <h3>Director: Dr.Saveetha Rajesh</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="<?php echo base_url()?>/images/leaders/Dr.P.Mangala Gowri.jpg" alt="">
+            <h3>Principal: Dr.P.Mangala Gowri</h3>
+          </div>
+        </div>
+        <!-- <h3 class="top30 bottom20">Our Leadership</h3>
+        <ul class="bottom25">
+          <li>Chancellor: Dr.N.MVeeraiyan</li>
+          <li>Vice Chancellor: Dr.Jawahar Nesan</li>
+          <li>Registrar: Dr.Thiyagarajan</li>
+          <li>Director: Dr.Saveetha Rajesh</li>
+          <li>Principal: Dr.P.Mangala Gowri</li>
+        </ul> -->
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Courses -->

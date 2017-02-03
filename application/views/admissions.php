c6eab0fb4288e57f227571b64727a985c02f128b
infantopratik <infanto.pratik@gmail.com>
2017-02-03 06:58:27 +0530

    Admissions - combined all submenus into a single view

---
+++ application/views/admissions.php
@@ -0,0 +1,124 @@
+
+
+<!--Page Header-->
+<section class="page_header child_nursing padding-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 page-content">
+        <h1>Admissions</h1>
+        <div class="page_nav">
+          <span>You are here:</span> <a href="<?php echo base_url() ?>">Home</a> <span><i class="fa fa-angle-double-right"></i>Admissions</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Page Header-->
+
+
+<!-- Courses -->
+<section id="course_all" class="padding-bottom-half padding-top">
+  <div class="container content-container">
+    <div class="row">
+      <div class="col-sm-10 col-sm-offset-1 course_detail wow fadeIn" data-wow-delay="400ms">
+        <!-- Carousel Starts -->
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/1.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/2.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/3.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/4.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/5.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/6.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/7.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img class="w-100 border_radius img-responsive hero-image" src="<?php echo base_url() ?>images/carousels/8.jpg" alt="...">
+				    </div>
+				  </div>
+
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+				<!-- Carousel Ends -->
+        <h3 class="top30 bottom20">Why Choose Us</h3>
+        <p class="bottom25">Saveetha College of Nursing offers hand full of experience in well-established infrastructure by experienced, competent faculty members to meet the global demand of the nurses. Institute has 1000 bedded multi speciality hospital within the campus. Hence no need for the students to go out for their clinical experience.  Hostel with all the amenities are available for boys and girls separately within the campus. Saveetha College of Nursing is unique in offering elective course and job oriented skill training for undergraduate students during the course period which provides more opportunity to place globally. Though the college premise is away from the city limit convenient transport facility is available in the institute.
+        </p>
+
+        <h3 class="top30 bottom20">Courses Offered</h3>
+        <ul class="bottom25">
+          <li>B.Sc.Nursing (4 Years)</li>
+          <li>Post Basic B.Sc. Nursing ( 2 Years, Eli: GNM)</li>
+          <li>M.Sc.Nursing  (2 Years)</li>
+          <li>&nbsp&nbsp&nbsp&nbsp 	Medical surgical nursing</li>
+          <li>&nbsp&nbsp&nbsp&nbsp 	Chid health nursing</li>
+          <li>&nbsp&nbsp&nbsp&nbsp 	Maternity nursing</li>
+          <li>&nbsp&nbsp&nbsp&nbsp 	Community health nursing</li>
+          <li>&nbsp&nbsp&nbsp&nbsp 	Mental health nursing / psychiatry nursing</li>
+        </ul>
+
+        <h3 class="top30 bottom20">Apply</h3>
+        <ul class="bottom25">
+          <li> 	Application and Prospectus can be obtained from admission office on the payment of Rs.500/- for UG courses & Rs.1000/- for PG courses</li>
+          <li> 	To download the application form and for further detail visit our website www.saveetha.com</li>
+          <li> 	Completed application should reach ‘Office of Admissions, Saveetha University’, Saveetha Nagar, Thandalam , Chennai-602105</li>
+        </ul>
+
+        <h3 class="top30 bottom20">Guidelines</h3>
+        <h4 class="top30 bottom20">B.Sc(Nursing)</h4>
+        <ul class="bottom25">
+          <li> 	The candidate should have completed the age of 17 years at the time of admission or will complete this age on 31st December of the year of admission.</li>
+          <li> 	The candidate should have completed the two years intermediate/ H.Sc or equivalent course thereof, with science subjects, viz. English, Physics, Chemistry and Biology from a recognized Indian University or Pre- University.</li>
+          <li> 	The candidate should have secured not less than 60% of marks on the aggregate of the above subjects in the qualifying or competitive examination conducted on similar lines as the qualifying examination conducted by a competitive body.</li>
+          <li> 	The candidate should be medically fit.</li>
+        </ul>
+        <h4 class="top30 bottom20">P.B.B.Sc(Nursing)</h4>
+        <ul class="bottom25">
+          <li> 	A candidate for admission to the Post Basic B.Sc. Nursing course should have passed General Nursing and Midwifery after 10, +2 or equivalent examination with science subjects. </li>
+          <li> 	The candidate should be a registered nurse and midwive or equivalent with any state nursing registration council.</li>
+          <li> 	A male nurse trained before the implementation of the new integrated course besides being registered as a nurse with State Nurses registration council, shall produce evidence of training approved by</li>
+          <li> 	Indian Nursing Council for a similar duration in lieu of midwifery in any one of the following areas – OT Techniques, Ophthalmic Nursing, Leprosy Nursing, TB Nursing, Psychiatric Nursing, Neurological and Neurosurgical Nursing, Community Health Nursing, Cancer Nursing, Orthopaedic Nursing.</li>
+          <li> 	Candidates should be medically fit.</li>
+        </ul>
+        <h4 class="top30 bottom20">M.Sc(Nursing)</h4>
+        <ul class="bottom25">
+          <li> 	A candidate for admission to the M.Sc. Degree in Nursing must possess a Degree of B.Sc. Nursing from a College / University duly recognized by the Indian Nursing Council with minimum 55% aggregate marks and should have one year of experience, after graduation.</li>
+          <li> 	The candidate should be medically fit.</li>
+          <li> 	In case of Male Candidates, they should have passed any other course recommended by the Indian Nursing Council in place of Midwifery. These male candidates are not eligible for admission to Branch III and Branch IV irrespective of their experience in these areas. Male candidates who have passed and registered in Midwifery is eligible for admission to branch V irrespective of their experience in these area.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Courses -->

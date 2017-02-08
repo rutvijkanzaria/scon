2e7a9e4e2517212019a53efe8b53b740d2f63024
Rutvij Kanzaria <rutvijkanzaria@gmail.com>
2017-02-08 15:42:47 +0530

    Admissions Modified

--- application/views/admissions.php
+++ application/views/admissions.php
@@ -75,6 +75,55 @@
         <h3 class="top30 bottom20">Why Choose Us</h3>
         <p class="bottom25">Saveetha College of Nursing offers hand full of experience in well-established infrastructure by experienced, competent faculty members to meet the global demand of the nurses. Institute has 1000 bedded multi speciality hospital within the campus. Hence no need for the students to go out for their clinical experience.  Hostel with all the amenities are available for boys and girls separately within the campus. Saveetha College of Nursing is unique in offering elective course and job oriented skill training for undergraduate students during the course period which provides more opportunity to place globally. Though the college premise is away from the city limit convenient transport facility is available in the institute.
         </p>
+		
+		
+		
+		
+		 <h3 class="top30 bottom20">Innovative Teaching Methods</h3>
+		 
+        <p class="bottom25"></p>
+		
+		 <h3 class="top30 bottom20">Small Group Learning</h3>
+		 <img src="<?php echo base_url() ?>images/why/smallgrouplearning.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+		 
+        <p class="bottom25">The students are divided into group and each group is guided by the facilitator to ensure group progress. Small group learning allows students to develop problem solving, interpersonal, presentational and communication skills, all beneficial to life outside the classroom. These generic skills are difficult to develop in isolation and require feedback and interaction with other individuals</p>
+		
+		 <h3 class="top30 bottom20">Context Based Learning</h3>
+		 
+		 <img src="<?php echo base_url() ?>images/why/CBLcommunity.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+		 
+        <p class="bottom25">The new approach of teaching and learning which was not introduced in our Nursing colleges is Context-based learning (CBL) in Community Health Nursing. It refers to the use of real-life and fictitious examples in “Teaching" environments in order to learn through the actual, practical experience with a subject rather than just its mere. The principles of CBL are that the starting point for learning should be a problem/situation and be student-centred with emphasis on self-directed learning. Students work in groups on real-life nursing situations. The group identifies and researches concepts and principles needed to work as a practicing nurse. The clinical scenarios encourage research, critical thinking, and developing lifelong learning skills.</p>
+		
+		 <h3 class="top30 bottom20">Multi Interactive Learning Algorithm (MILA)</h3>
+		 
+		 <img src="<?php echo base_url() ?>images/why/MILAPhotos.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+        <p class="bottom25">Introduced Multi Interactive Learning Algorithm (MILA) for B.Sc(N) I year students in Nursing Foundation Subject. This method of teaching This method consists of Twenty minutes of teaching time followed by a Twenty minute activity time to better understand and learn the concept. Activities such as jigsaw, POGIL(Process Oriented Guided Inquiry Learning), mind mapping, game based learning, critical pedagogy, peer-led team based learning, role play and scale up. This method of teaching was very interactive, attracts the student attention, retains the memory of the content and makes the student to search for books, journals and internet to find the solution to the questions.</p>
+		
+		
+		<h3 class="top30 bottom20">E-Learning</h3>
+		
+		<img src="<?php echo base_url() ?>images/why/E-learning.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+        <p class="bottom25">E-learning resources provided to undergraduate and post graduate students to promote self learning and widen the knowledge. It broadens the horizons of many students as it exposes students to the world outside their city or country town. This method creates learn not to rely on the teacher, accountable themselves and become independent.</p>
+		
+		<h3 class="top30 bottom20">Self Assessment Module</h3>
+		
+		<img src="<?php echo base_url() ?>images/why/Selfassessmentmodule.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+        <p class="bottom25">Self Assessment module is one the method of self evaluation of the students as well which create the critical thinking and better understand of their own. It evolves in response to need based assessment, investigations and therapeutic management  providing an opportunity for the undergraduate students for  proficiency in patient management skills.</p>
+		
+		<h3 class="top30 bottom20">Self Learning Desk</h3>
+		
+		<img src="<?php echo base_url() ?>images/why/SelfLearning.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+        <p class="bottom25">The steps of procedure with principles is prepared and displayed in the lab and students read and do the procedure to the manikin. This method develops the skill and competent among the students.</p>
+		
+		<h3 class="top30 bottom20">Simulation Corner</h3>
+		<img src="<?php echo base_url() ?>images/why/Simulationcorner.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+		
+        <p class="bottom25">Simulation corner is designed to give students hands-on clinical experience. All Laboratories are equipped with simulation model to practice the procedures before they practice on the patients and it creates real life experience and develops skills</p>
+      
+	  <h3 class="top30 bottom20">Inter-Professional Learning</h3>
+	  
+	  <img src="<?php echo base_url() ?>images/why/InterprofessionalPosterpresentation.jpg" alt="Course" class=" border_radius img-responsive bottom15">
+        <p class="bottom25">Students from different disciplines who learnt together developed interpersonal and teamwork skills and gained knowledge of how other professionals work. The healthcare professional may have been exposed to situations in training that can be reflected on, they know how to behave towards other staff and should have good communication skills that will help them work well in a team. The IPL is to prepare students with the interprofessional skills that will later form the core of their professional identity and pave a smoother route to optimal patient care.</p>
 
         <h3 class="top30 bottom20">Courses Offered</h3>
         <ul class="bottom25">
